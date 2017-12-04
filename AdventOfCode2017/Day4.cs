2d5de50e19749b5c5b19f2fea089df8099d787f1
Pieter Demaegdt <pieter.demaegdt.ext@ts.fujitsu.com>
2017-12-04 08:18:58 +0100

    Day4Part1

---
+++ AdventOfCode2017/Day4.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace AdventOfCode2017
+{
+    class Day4
+    {
+        internal static void Part1()
+        {
+            FileInfo inputfile = new FileInfo(Program.dir + "Day4.txt");
+            var stream = inputfile.OpenText();
+            int validPassphrases = 0;
+            while (!stream.EndOfStream)
+            {
+                List<string> words = new List<string>();
+                foreach (var item in stream.ReadLine().Split(' '))
+                {
+                    if (words.Contains(item))
+                    {
+                        break;
+                    } else
+                    {
+                        words.Add(item);
+                    }
+                }
+                validPassphrases++;
+            }
+            Console.WriteLine("Valid passphrases: " + validPassphrases);
+
+        }
+
+        internal static void Part2()
+        {
+            //throw new NotImplementedException();
+        }
+    }
+}
--- AdventOfCode2017/Program.cs
+++ AdventOfCode2017/Program.cs
@@ -8,8 +8,8 @@ namespace AdventOfCode2017
         public static string dir = @"C:\Users\piete\source\repos\AdventOfCode2017\AdventOfCode2017\input\";
         static void Main(string[] args)
         {
-            Day2.Part1();
-            Day2.Part2();
+            Day4.Part1();
+            Day4.Part2();
             Console.ReadLine();
         }
     }
